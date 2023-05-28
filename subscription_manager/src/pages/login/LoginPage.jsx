0c99cbb121a6dded4bcad1ad9e0c41eaa079e893
Sergei <amatory365@gmail.com>
2023-05-28 20:36:28 +0300

    authorization page starting

---
+++ subscription_manager/src/pages/login/LoginPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import modules from '../../style/login.module.scss'
+
+const LoginPage = () => {
+
+    return(
+        <div className={modules.backGroundLogin}>
+            <div className={modules.loginWrapper}>
+                <h2 className={modules.headerLabel}>Авторизация</h2>
+                <input className={modules.inputBox} placeholder="Логин" />
+                <input className={modules.inputBox} placeholder="Пароль" type="password"/>
+                <button className={modules.buttonBox}>Войти</button>
+                <span>Нет учётной записи? <a href=''>Регистрация</a></span>
+            </div>
+        </div>
+    )
+}
+
+export default LoginPage;
